bc6840586b6d56ce922adb0a5a8109f0d3fd8087
zolboo-gtn <72186937+zolboo-gtn@users.noreply.github.com>
2025-08-06 14:23:07 +0800

    set badge

--- src/app/fcm.tsx
+++ src/app/fcm.tsx
@@ -11,10 +11,21 @@ export const FCM = () => {
   const notificationPermission = useAtomValue(notificationPermissionAtom);
 
   return (
-    <div>
+    <div className="flex flex-col items-start gap-2">
       <div>FCM Token: {fcmToken}</div>
       <div>Notification Permission: {notificationPermission}</div>
-      <button onClick={() => setBadge(1)}>Set Badge</button>
+      <button onClick={() => setBadge(1)}>Set favicon badge</button>
+      <button
+        onClick={async () => {
+          if (!("setAppBadge" in navigator)) {
+            alert("setAppBadge is not supported in this browser");
+            return;
+          }
+          await navigator.setAppBadge(1);
+        }}
+      >
+        Set Badge
+      </button>
     </div>
   );
 };
